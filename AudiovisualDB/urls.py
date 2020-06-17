4aa39b085247e307a303fac651f3e325b7b974b2
mortersen <mortersen@163.com>
2020-06-17 18:21:09 +0800

    add audiovisual_html

--- AudiovisualDB/urls.py
+++ AudiovisualDB/urls.py
@@ -7,9 +7,9 @@ app_name = 'AudiovisualDB'
 
 urlpatterns = [
     path('',audiovisualdb_main,name='audiovisualdb_main'),#口述史料库主页
-    #path('operatext/',operatext,name='operatext'),#戏曲文本
+    path('video/',video,name='video'),#戏曲文本
     #path('operatext/<int:id>/',operatext_detail,name='operatext_detail'),#戏曲文本按ID号显示详细页
-    #path('folksong/',folksong,name='folksong'),#歌谣,处理翻页
+    path('audio/',audio,name='audio'),#歌谣,处理翻页
     #path('folksong/<int:id>/',folksong_detail,name='folksong_detail'),#歌谣按ID号显示详细页
 
 ]
--- AudiovisualDB/views.py
+++ AudiovisualDB/views.py
@@ -12,4 +12,41 @@ def audiovisualdb_main(request):
     return render(request,'audiovisualdb/audiovisualdb_main.html',target)
 
 
+def video(request):
+    list = Video.objects.all()
+    page = request.GET.get('page')
+    # print(page)
+    paginator = Paginator(list, 8)
 
+    try:
+        cur_dissertation = paginator.page(page)
+
+    except PageNotAnInteger:
+        cur_dissertation = paginator.page(1)
+    except EmptyPage:
+        cur_dissertation = paginator.page(paginator.num_pages)
+        # print(cur_dissertation.paginator.count)
+        # print(cur_dissertation.number)
+
+
+    return render(request,'audiovisualdb/video.html',{'cur_dissertation':cur_dissertation,})
+
+
+
+def audio(request):
+    list = Video.objects.all()
+    page = request.GET.get('page')
+    # print(page)
+    paginator = Paginator(list, 8)
+
+    try:
+        cur_dissertation = paginator.page(page)
+
+    except PageNotAnInteger:
+        cur_dissertation = paginator.page(1)
+    except EmptyPage:
+        cur_dissertation = paginator.page(paginator.num_pages)
+        # print(cur_dissertation.paginator.count)
+        # print(cur_dissertation.number)
+
+    return render(request,'audiovisualdb/audio.html',{'cur_dissertation':cur_dissertation,})
