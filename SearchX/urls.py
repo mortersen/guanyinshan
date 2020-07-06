fa7c917cfa918bff4007a371facb0668cf4b61fd
mortersen <mortersen@163.com>
2020-07-06 18:25:48 +0800

    SEARCHXXXX

--- SearchX/urls.py
+++ SearchX/urls.py
@@ -8,5 +8,6 @@ app_name = 'SearchX'
 
 urlpatterns = [
     path('',searchx_main,name='searchx_main'),#搜索主页
+    path('searchXresual/',searchXresual,name='searchXresual'),#搜索结果页面
 
 ]
--- SearchX/views.py
+++ SearchX/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
+from DocumentDB.models import Books,Periodical,Dissertation,ConferencePapers
+from DictateDB.models import OperaText,FolkSong,SongBook,SouthernMusic
+from AudiovisualDB.models import Audio,Video
+
 
 # Create your views here.
 
@@ -6,3 +11,61 @@ def searchx_main(request):
 
     target = {'':'',}
     return render(request, 'searchx/searchx.html', target)
+
+def searchXresual(request):
+    if request.method == 'POST':
+        sdb = request.POST.get("sdb")
+        skey = request.POST.get("skey").replace(' ','').replace(' ','')
+    if skey == '':
+        return render(request,'searchx/searchx.html',)
+    print(sdb, skey)
+
+    if sdb == 'TS':
+        searchlist = Books.objects.filter(Title__contains=skey)
+        sourcepath = 'DocumentDB:books_detail'
+    elif sdb == 'QK':
+        searchlist = Periodical.objects.filter(Title__contains=skey)
+        sourcepath = 'DocumentDB:periodical_detail'
+    elif sdb == 'XWLW':
+        searchlist = Dissertation.objects.filter(Title__contains=skey)
+        sourcepath = 'DocumentDB:dissertation_detail'
+    elif sdb == 'HYLW':
+        searchlist = ConferencePapers.objects.filter(Title__contains=skey)
+        sourcepath = 'DocumentDB:conferencepapers_detail'
+    elif sdb == 'XQWB':
+        searchlist = OperaText.objects.filter(Title__contains=skey)
+        sourcepath = 'DictateDB:operatext_detail'
+    elif sdb == 'GY':
+        searchlist = FolkSong.objects.filter(Title__contains=skey)
+        sourcepath = 'DictateDB:folksong_detail'
+    elif sdb == 'GC':
+        searchlist = SongBook.objects.filter(Title__contains=skey)
+        sourcepath = 'DictateDB:songbook_detail'
+    elif sdb == 'NYQP':
+        searchlist = SouthernMusic.objects.filter(Title__contains=skey)
+        sourcepath = 'DictateDB:southernmusic_detail'
+    elif sdb == 'SP':
+        searchlist = Video.objects.filter(Title__contains=skey)
+        sourcepath = 'AudiovisualDB:video'
+    elif sdb == 'YP':
+        searchlist = Audio.objects.filter(Title__contains=skey)
+        sourcepath = 'AudiovisualDB:audio'
+
+    if searchlist.count() == 0:
+        return HttpResponse("查询结果为空！")
+
+    paginator = Paginator(searchlist, 8)
+    page = request.GET.get('page')
+    print(page)
+    try:
+        target = paginator.page(page)
+
+    except PageNotAnInteger:
+        target = paginator.page(1)
+    except EmptyPage:
+        target = paginator.page(paginator.num_pages)
+
+    for item in target:
+        print(item.Title)
+
+    return render(request,'searchx/searchlist.html',{'cur_dissertation':target,'sourcepath':sourcepath})
