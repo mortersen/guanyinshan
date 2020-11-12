7da249901da234a6d15ae8376675fcb397183e30
unknown <mortersen@163.com>
2020-11-12 20:01:50 +0800

    添加留言板数据模版

--- OnlineConsult/views.py
+++ OnlineConsult/views.py
@@ -5,9 +5,9 @@ from .models import Consult
 def consult_main(request ):
     ctx = {}
     if request.method == 'POST':
-        title = request.POST.get("title")
-        context = request.POST.get("context")
-        contact = request.POST.get("contact")
+        title = request.POST.get("title").strip()
+        context = request.POST.get("context").strip()
+        contact = request.POST.get("contact").strip()
         print(title,context,contact)
         if title == "":
             ctx['rlt'] = "标题不能为空！"
@@ -16,6 +16,8 @@ def consult_main(request ):
         elif contact == "":
             ctx['rlt'] = "请留下联系方式！"
         else:
+            data = Consult(Title=title,Context=context,Contact=contact)
+            data.save()
             ctx['rlt'] = "咨询内容已提交！"
 
     return render(request,'OnlineConsult/consult.html',ctx)
